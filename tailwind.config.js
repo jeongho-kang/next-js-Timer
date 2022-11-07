1ed6234a8c4df82f6e90df95c9f04679fa85c7ba
jeongho-kang <aj9037@naver.com>
2022-11-07 17:21:21 +0900

    1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { isIdentifierOrPrivateIdentifier } = require('typescript');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,7 +8,20 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        flip:'flip is cubic-bezier (0,0,0.2,1) infinite'
+      },
+      keyframes: {
+        flip: {
+          'from':{transform:'rotateX(0deg)', transformOrigin: '50% bottom',},
+          'to':{transform: 'rotateX(180deg)', transformOrigin:'50% bottom',}
+        }
+      }
+    },
+    fontFamily: {
+      'redhat':['Red Hat Text', 'sans-serif'],
+    }
   },
   plugins: [],
 }
